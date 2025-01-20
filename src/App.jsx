e372f24eb2d2dd7a90d5f3a0c499a8a1eaea1753
bschloss <benschloss2@gmail.com>
2025-01-20 13:33:15 -0500

    Adding incomplete sign up form. To be finished.

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 // src/App.jsx
+import { Routes, Route } from 'react-router';
 
 import NavBar from './components/NavBar/NavBar.jsx';
+import SignUpForm from './components/SignUpForm/SignUpForm.jsx';
 
 const App = () => {
-  
+
   return (
     <>
       <NavBar />
-      <h1>Hello, friend!</h1>
+      <Routes>
+        <Route path='/sign-up' element={<SignUpForm />} />
+      </Routes>
     </>
   );
 };
