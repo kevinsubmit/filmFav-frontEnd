2fed904048a4a7b25ffd357366c21ef9730f940a
bschloss <benschloss2@gmail.com>
2025-01-21 18:32:53 -0500

    adding incomplete first version of navbar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,32 +1,33 @@
-// src/components/NavBar/NavBar.jsx
 import { useContext } from 'react';
 import { AuthedUserContext } from '../../App';
 import { Link } from 'react-router-dom';
+import './NavBar.css';
 
-const NavBar = ({ handleSignout}) => {
-    
-    const user = useContext(AuthedUserContext)
+const NavBar = ({ handleSignout }) => {
+  const user = useContext(AuthedUserContext);
 
   return (
     <>
-      { user ? (
-        <nav>
-          <ul>
-            <li><Link to="/">Home</Link></li>
-            <li><Link to='/movies'>Movies</Link></li>
-            <li><Link to="" onClick={handleSignout}>Sign Out</Link></li>
+      {user ? (
+        <nav className="navbar">
+          <ul className="nav-links">
+            <li><Link to="/" className="nav-link"><img src="https://github.com/bschlo/filmFav-frontEnd/blob/LandingPage/src/assets/Film%20Favorite.png?raw=true" alt="logo" /></Link></li>
+            <li><Link to="/movies" className="nav-link">Movies</Link></li>
+            <li><Link to="" onClick={handleSignout} className="nav-link">Sign Out</Link></li>
           </ul>
         </nav>
       ) : (
-        <nav>
-          <ul>
-            <li><Link to="/signin">Sign In</Link></li>
-            <li><Link to="/signup">Sign Up</Link></li>
+        <nav className="navbar">
+          <ul className="nav-links">
+          <li><Link to="/" className="nav-link"><img src="https://github.com/bschlo/filmFav-frontEnd/blob/LandingPage/src/assets/Film%20Favorite.png?raw=true" alt="logo" /></Link></li>
+            <li><Link to="/signin" className="nav-link">Sign In</Link></li>
+            <li><Link to="/signup" className="nav-link">Sign Up</Link></li>
           </ul>
         </nav>
       )}
     </>
-  )
-}
+  );
+};
 
 export default NavBar;
+
