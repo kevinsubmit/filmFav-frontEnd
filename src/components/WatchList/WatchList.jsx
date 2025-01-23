c00e060bc434a8bf4a0848b148fb6f31e9293cdb
viededario <darioocceus@gmail.com>
2025-01-23 18:08:39 -0500

    began WatchList

--- src/components/WatchList/WatchList.jsx
+++ src/components/WatchList/WatchList.jsx
@@ -1,6 +1,7 @@
 import { Link } from "react-router";
 import React, { useState, useEffect } from 'react';
 import * as mineService from "../../services/mineService";
+import './WatchList.css';
 
 const WatchList = () => {
   const [watchList, setwatchList] = useState({});
@@ -27,17 +28,14 @@ const WatchList = () => {
     };
 
   return (
-    <ul>
+    <ul className="watchlist">
       {watchList.length > 0 ? (
         watchList.map((movie) => (
-          <li key={movie.id}>
+          <li key={movie.id} className="watchlist-movie-card">
             <article>
-              <h2>{movie.title}</h2>
-              <img src={movie.poster_url} alt="poster_img" />
-              <p>{movie.description}</p>
-              <Link to={`/movies/${movie.id}`}>View Details</Link>
+              <Link to={`/movies/${movie.id}`} className="movie-link"><img src={movie.poster_url} alt="poster_img" className="watch-img" /></Link>
               <div>
-                <button onClick={() => removeFromMyWatchlist([movie.id])}>
+                <button onClick={() => removeFromMyWatchlist([movie.id])} className="remove-btn">
                 remove From MyWatchlist
                 </button>
               </div>
