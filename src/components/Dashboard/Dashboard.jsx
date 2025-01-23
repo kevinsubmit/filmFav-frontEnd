76bd423bcf6e6b215e27a62742c24e94b865fb96
Joemer <joemers97@gmail.com>
2025-01-23 15:03:53 -0500

    Style updates for landing page, added logo, modified Navbar component, updated index.css, and added signup link. Landing page functionality complete.

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -2,21 +2,19 @@ import { useContext } from 'react';
 import { AuthedUserContext } from '../../App';
 
 const Dashboard = () => {
-    // const user = useContext(AuthedUserContext);
-    const user = localStorage.getItem('user')
-    console.log(user)
-    
+	// const user = useContext(AuthedUserContext);
+	const user = localStorage.getItem('user');
+	console.log(user);
 
-    return (
-      <main>
-        <h1>Welcome, {user}</h1>
-        <p>
-          This is the dashboard page where you, and only you, can see a dashboard
-          of all of your things.
-        </p>
-      </main>
-    );
-  };
-  
-  export default Dashboard;
-  
+	return (
+		<main>
+			<h1>Welcome, {user}</h1>
+			<p>
+				This is the dashboard page where you, and only you, can see a dashboard
+				of all of your things.
+			</p>
+		</main>
+	);
+};
+
+export default Dashboard;
--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -1,15 +1,29 @@
-// src/components/Landing.jsx
+import { Link } from 'react-router-dom';
+import './Landing.css';
 
 const Landing = () => {
-    return (
-      <main>
-        <h1>Hello, you are on the landing page for visitors.</h1>
-        <h3>
-          If you sign up for a new account, you will have the ability to sign in
-          and see your super secret dashboard.
-        </h3>
-      </main>
-    );
-  };
-  
-  export default Landing;
+	return (
+		<main>
+			<img className='logo_page' src='/public/logo_page.png' alt='logo' />
+
+			<h1>
+				Welcome to FilmFav – Your Go-To Hub for Discovering and Sharing Movies!
+			</h1>
+
+			<h3>
+				Explore cinema with FilmFav! <span />
+				Create lists, discover trends, and connect with movie lovers.
+				<br />
+				<strong className='signup_link'>
+					<Link to='/signup' style={{ color: 'white' }}>
+						{' '}
+						Sign Up
+					</Link>
+				</strong>
+				<span> to make every movie moment unforgettable!</span>
+			</h3>
+		</main>
+	);
+};
+
+export default Landing;
