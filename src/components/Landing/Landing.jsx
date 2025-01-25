2f27c774fd431d409f1dabcec0b8356fe889194d
Joemer <joemers97@gmail.com>
2025-01-25 18:00:46 -0500

    enhance landing page design for improved aesthetics and user engagement

--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -1,5 +1,6 @@
 import { Link } from 'react-router-dom';
 import './Landing.css';
+import './Landing.css';
 
 const Landing = () => {
 	return (
@@ -9,15 +10,51 @@ const Landing = () => {
 			<h1>
 				Welcome to FilmFav – Your Go-To Hub for Discovering and Sharing Movies!
 			</h1>
+			<br />
+
+			<div className='video-row'>
+				<video
+					src='/public/videos/compressed_BEN 10 Live Action Movie – First Trailer   Tom Holland   Netflix (4K).mp4'
+					autoPlay
+					loop
+					muted
+				/>
+				<video
+					src='/public/videos/compressed_Optimus vs Bumble.mp4'
+					autoPlay
+					loop
+					muted
+				/>
+				<video src='/public/videos/compressed_topgun.mp4' autoPlay loop muted />
+				<video src='/public/videos/compressed_Sonic3.mp4' autoPlay loop muted />
+				<video
+					src='/public/videos/compressed_Superman12.mp4'
+					autoPlay
+					loop
+					muted
+				/>
+				<video
+					src='/public/videos/compressed_Transpor_shorter.mp4'
+					autoPlay
+					loop
+					muted
+				/>
+				<video
+					src='/public/videos/compressed_How To Train Your Dragon   Official Teaser Trailer.mp4'
+					autoPlay
+					loop
+					muted
+				/>
+			</div>
 
 			<h3>
 				Explore cinema with FilmFav! <span />
 				Create lists, discover trends, and connect with movie lovers.
 				<br />
 				<strong className='signup_link'>
-					<Link to='/signup' style={{ color: 'white' }}>
+					<Link to='/signup'>
 						{' '}
-						Sign Up
+						<span className='span-signup'>Sign Up</span>
 					</Link>
 				</strong>
 				<span> to make every movie moment unforgettable!</span>
